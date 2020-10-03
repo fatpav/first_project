a1815599e392c54d45c8066ba6647e1867275049
fatpav <fat_pav@hotmail.com>
2020-10-03 15:47:53 +0100

    user repository and console started

--- console.py
+++ console.py
@@ -7,3 +7,8 @@ from models.team import Team
 import repositories.fixture_repository as fixture_repository
 import repositories.team_repository as team_repository
 import repositories.fixture_repository as fixture_repository
+
+user_repository.delete_all()
+
+user1 = User("Paul Donegan")
+user_repository.save(user1)
--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -1,2 +1,4 @@
 from flask import Flask, render_template, request, redirect
+
 from flask import Blueprint
+
