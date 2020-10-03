1293430897d3629c015b0007b73802890643ae3e
fatpav <fat_pav@hotmail.com>
2020-10-03 15:30:46 +0100

    flaskenvironment up and running nicely

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return Hello Peep head
+    return "Hello Peep head"
 
 
 if __name__ == '__main__':
--- console.py
+++ console.py
@@ -6,4 +6,4 @@ from models.team import Team
 
 import repositories.fixture_repository as fixture_repository
 import repositories.team_repository as team_repository
-import repositories
+import repositories.fixture_repository as fixture_repository
