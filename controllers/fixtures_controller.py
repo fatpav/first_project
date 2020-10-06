64aa2e823b2a15726d28b6027ab95787f8b6d272
fatpav <fat_pav@hotmail.com>
2020-10-06 21:16:27 +0100

    back to where I was 2 hours ago. joy

--- controllers/fixtures_controller.py
+++ controllers/fixtures_controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, Blueprint
 
 from models.fixture import Fixture
+from models.team import Team
 
 
 import repositories.team_repository as team_repository
